e528fdcdc0a79c98c72a7f39d40177c36e9a1491
loft897 <salazar37@protonmail.com>
2022-03-24 08:10:28 +0000

    tout ok

--- Front End/dist/main.bundle.js
+++ Front End/dist/main.bundle.js
@@ -12,26 +12,25 @@ function fecthTeddies() {
   })["catch"](function (error) {
     info.innerHTML = "  Nous n'avons pas r\xE9ussi \xE0 afficher nos produits. Marci de lancer le\n        serveur via le port 3000! <br />Si le probl\xE8me persite, prendre\n        contact avec le support.";
   }).then(function (allTeddies) {
-    console.log(allTeddies);
-    createTeddie(allTeddies);
+    createDisplayTeddies(allTeddies);
   });
 }
 
-fecthTeddies(); // Creation de chaque produit
+fecthTeddies(); // Creation et affichage des produits
 
-function createTeddie(allTeddies) {
-  info.innerHTML = "  Nos produits";
-  info.style.fontSize = "30px";
-
+function createDisplayTeddies(allTeddies) {
   for (var teddie in allTeddies) {
+    info.innerHTML = "  Nos produits";
+    info.style.fontSize = "30px";
     var product = document.createElement("div");
     product.classList.add("card-product");
     var linkProduct = document.createElement("a");
-    linkProduct.href = "./Front end/pages/product.html?id=".concat(allTeddies[teddie]._id);
+    linkProduct.href = "pages/product.html?id=".concat(allTeddies[teddie]._id);
     var divImgProduct = document.createElement("div");
     divImgProduct.classList.add("case-img");
     var imgProduct = document.createElement("img");
     imgProduct.src = allTeddies[teddie].imageUrl;
+    imgProduct.alt = "image de la peluche ".concat(allTeddies[teddie].name);
     var divInfoProduct = document.createElement("div");
     divInfoProduct.classList.add("case-info");
     var nameProduct = document.createElement("h4");
@@ -50,7 +49,7 @@ function createTeddie(allTeddies) {
   }
 }
 
-createTeddie();
+createDisplayTeddies();
 /******/ })()
 ;
 //# sourceMappingURL=main.bundle.js.map
